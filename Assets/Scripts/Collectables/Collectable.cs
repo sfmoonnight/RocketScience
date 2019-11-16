9a5054cc96d382b0c5e865854deb1a777e73695a
Vivi Ma <vinjiminster@gmail.com>
2019-11-16 12:09:24 -0500

    started planet and collectables

---
+++ Assets/Scripts/Collectables/Collectable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public int identity;
+    public float rareness;
+    public bool pickable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivatePickUp()
+    {
+        pickable = true;
+    }
+
+    public void DeactivatePickUp()
+    {
+        pickable = false;
+    }
+}
---
+++ Assets/Scripts/Planet/Planet.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    public int answer;
+    public int capacity;
+    public bool activate;
+    public List<Collectable> options;
+    public List<Collectable> collectables;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Toolbox.GetInstance().GetGameManager().answer == answer)
+        {
+            if (!activate)
+            {
+                ActivateCollectables();
+            }
+            activate = true;
+        }
+        else
+        {
+            if (activate)
+            {
+                DeactivateCollectables();
+            }
+            activate = false;
+        }
+    }
+
+    void GenerateCollectables()
+    {
+        foreach(Collectable col in options)
+        {
+            float rate = Random.Range(0f, 1f);
+            if(rate < col.rareness)
+            {
+                Collectable newCol = Instantiate(col);
+                collectables.Add(newCol);
+            }
+        }
+    }
+
+    void ActivateCollectables()
+    {
+        foreach (Collectable col in collectables)
+        {
+            col.ActivatePickUp();
+        }
+    }
+
+    void DeactivateCollectables()
+    {
+        foreach (Collectable col in collectables)
+        {
+            col.DeactivatePickUp();
+        }
+    }
+}
