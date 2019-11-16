bff51a2673c2eb35c1d04e1b1afa52e0b3012dd9
Jimmy Li <vanjiminster@gmail.com>
2019-11-16 13:38:23 -0500

    added equation generator

---
+++ Assets/Scripts/Planet/EquationTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquationTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void test()
+    {
+        Addition1 eq = new Addition1();
+        print(eq.toString());
+    }
+}
